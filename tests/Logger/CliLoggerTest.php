721ec8aaa06de77322191440ea1be4aed0b4f931
otsch <otsch@users.noreply.github.com>
2022-02-06 23:47:47 +0100

    Add tests for the CliLogger

---
+++ tests/Logger/CliLoggerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace tests\Logger;
+
+use Crwlr\Crawler\Logger\CliLogger;
+
+test('It prints a message', function () {
+    $logger = new CliLogger();
+    $logger->log('info', 'Some log message.');
+    $output = $this->getActualOutput();
+    expect($output)->toContain('Some log message.');
+});
+
+test('It prints the log level', function () {
+    $logger = new CliLogger();
+    $logger->log('alert', 'Everybody panic!');
+    $output = $this->getActualOutput();
+    expect($output)->toContain('[ALERT]');
+});
+
+test('It starts with printing the time', function () {
+    $logger = new CliLogger();
+    $logger->log('warning', 'Warn about something.');
+    $this->expectOutputRegex('/^\d\d:\d\d:\d\d:\d\d\d\d\d\d/');
+});
+
+test('It has methods for all the log levels', function ($logLevel) {
+    $logger = new CliLogger();
+    $logger->{$logLevel}('Some message');
+    $output = $this->getActualOutput();
+    expect($output)->toContain('Some message');
+    expect($output)->toContain('[' . strtoupper($logLevel) . ']');
+})->with([
+    'emergency',
+    'alert',
+    'critical',
+    'error',
+    'warning',
+    'notice',
+    'info',
+    'debug',
+]);
