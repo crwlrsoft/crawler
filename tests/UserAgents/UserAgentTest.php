7a7c2f1aa970e1a37698d32c9383d15d11cdc824
otsch <otsch@users.noreply.github.com>
2022-02-24 13:55:02 +0100

    Add a test for the new simple UserAgent class

---
+++ tests/UserAgents/UserAgentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace tests\UserAgents;
+
+use Crwlr\Crawler\UserAgents\UserAgent;
+
+test(
+    'It can be created with any string in constructor and the __toString method returns that string',
+    function ($string) {
+        $userAgent = new UserAgent($string);
+        expect($userAgent->__toString())->toBe($string);
+    }
+)->with([
+    '',
+    'Foo',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 ' .
+    'Safari/537.36',
+    '%$§$!")(=aäöüäö?ßß``2304980=)(§$/&!"=)=',
+]);
