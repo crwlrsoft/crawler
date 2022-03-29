455f43a909c4987bb8cffd781f5bd3c98736888f
otsch <otsch@users.noreply.github.com>
2022-03-29 19:26:29 +0200

    Remove the QuereSelector classes
    And add static methods for easier usage of the GetLink(s) step classes.

--- src/Steps/Html.php
+++ src/Steps/Html.php
@@ -4,9 +4,21 @@ namespace Crwlr\Crawler\Steps;
 
 use Crwlr\Crawler\Steps\Html\CssSelector;
 use Crwlr\Crawler\Steps\Html\DomQueryInterface;
+use Crwlr\Crawler\Steps\Html\GetLink;
+use Crwlr\Crawler\Steps\Html\GetLinks;
 
 class Html extends Dom
 {
+    public static function getLink(string $selector): GetLink
+    {
+        return new GetLink($selector);
+    }
+
+    public static function getLinks(string $selector): GetLinks
+    {
+        return new GetLinks($selector);
+    }
+
     protected function makeDefaultDomQueryInstance(string $query): DomQueryInterface
     {
         return new CssSelector($query);
