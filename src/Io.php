3cda44aeb35c6e4f05d092e2e4e3cead1af3eeeb
Viktor Szépe <viktor@szepe.net>
2022-06-17 09:03:02 +0000

    Improve Io::__construct even more

--- src/Io.php
+++ src/Io.php
@@ -4,17 +4,13 @@ namespace Crwlr\Crawler;
 
 class Io
 {
-    protected mixed $value;
-
-    public function __construct(mixed $value, public ?Result $result = null)
+    public function __construct(protected mixed $value, public ?Result $result = null)
     {
         if ($value instanceof self) {
             $this->value = $value->value;
-            $this->result ??= $value->result;
-            return;
-        }
 
-        $this->value = $value;
+            $this->result ??= $value->result;
+        }
     }
 
     public function get(): mixed
