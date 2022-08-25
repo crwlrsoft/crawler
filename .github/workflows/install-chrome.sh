76aab09b7cafd20ac53151f6c061b5e2170c3a79
otsch <otsch@users.noreply.github.com>
2022-08-25 02:59:52 +0200

    Remove unnecessary shell script

--- .github/workflows/install-chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:canonical-chromium-builds/stage
-sudo apt-get update
-sudo apt-get install chromium-browser
-chromium-browser --headless --no-sandbox http://example.org/
