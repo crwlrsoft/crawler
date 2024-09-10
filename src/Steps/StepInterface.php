8da659661ce0ea2a1e023165ae4d4c3467491cad
otsch <otsch@users.noreply.github.com>
2024-09-10 22:10:22 +0200

    Add the keep methods to the StepInterface

--- src/Steps/StepInterface.php
+++ src/Steps/StepInterface.php
@@ -18,6 +18,26 @@ interface StepInterface
      */
     public function invokeStep(Input $input): Generator;
 
+    /**
+     * @param string|string[]|null $keys
+     */
+    public function keep(string|array|null $keys = null): static;
+
+    public function keepAs(string $key): static;
+
+    /**
+     * @param string|string[]|null $keys
+     */
+    public function keepFromInput(string|array|null $keys = null): static;
+
+    public function keepInputAs(string $key): static;
+
+    public function keepsAnything(): bool;
+
+    public function keepsAnythingFromInputData(): bool;
+
+    public function keepsAnythingFromOutputData(): bool;
+
     public function useInputKey(string $key): static;
 
     public function uniqueInputs(?string $key = null): static;
