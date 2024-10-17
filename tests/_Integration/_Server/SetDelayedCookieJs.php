ec310ca7ee6f2b4a7091a75d7f46117aeb374a9e
otsch <otsch@users.noreply.github.com>
2024-10-17 23:56:02 +0200

    Post Browser Navigate Hooks
    Adds post browser navigate hooks that are passed from `Http` steps to
    the browser loader helper and are executed after the headless browser
    navigated to the specified URL. They are called with the chrome-php
    `Page` object as argument, so you can run things on that page before
    getting the HTML and returning the response.

---
+++ tests/_Integration/_Server/SetDelayedCookieJs.php
@@ -0,0 +1,10 @@
+<?php setcookie('testcookie', 'foo123'); ?>
+<!doctype html>
+<html lang="de">
+<head><meta charset=utf-8><title>Hey</title></head>
+<body>
+<div>
+    <button id="setCookieButton" onclick="document.cookie = 'testcookie=jscookie'"></button>
+</div>
+</body>
+</html>
