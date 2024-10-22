208b29f240d1d30f4e166ae969ca3b9d19c4eab3
otsch <otsch@users.noreply.github.com>
2024-10-22 12:04:55 +0200

    Fix converting chrome-php cookie objects
    Fixes issue when converting cookie objects received from the chrome-php
    library.

--- tests/Loader/Http/Cookies/CookieJarTest.php
+++ tests/Loader/Http/Cookies/CookieJarTest.php
@@ -48,16 +48,52 @@ test('addFrom() works with a CookieCollection from the chrome-php lib', function
     $jar = new CookieJar();
 
     $jar->addFrom(Url::parse('https://www.crwl.io'), new CookiesCollection([
-        new Cookie(['name' => 'foo', 'value' => 'one', 'domain' => '.www.crwl.io', 'expires' => '1745068860']),
-        new Cookie(['name' => 'bar', 'value' => 'two', 'domain' => '.www.crwl.io', 'expires' => '1729603260.5272']),
-        new Cookie(['name' => 'baz', 'value' => 'three', 'domain' => '.www.crwl.io', 'expires' => '1764076860.878']),
+        new Cookie([
+            'name' => 'foo',
+            'value' => 'one',
+            'domain' => '.www.crwl.io',
+            'expires' => '1745068860',
+            'max-age' => '86400',
+            'secure' => true,
+            'httpOnly' => true,
+            'sameSite' => 'Strict',
+        ]),
+        new Cookie([
+            'name' => 'bar',
+            'value' => 'two',
+            'domain' => '.www.crwl.io',
+            'expires' => '1729603260.5272',
+            'path' => '/bar',
+        ]),
+        new Cookie([
+            'name' => 'baz',
+            'value' => 'three',
+            'domain' => '.www.crwl.io',
+            'expires' => '1764076860.878',
+        ]),
     ]));
 
     $allCookiesForDomain = $jar->allByDomain('crwl.io');
 
     expect($allCookiesForDomain)->toHaveCount(3)
         ->and($allCookiesForDomain['foo']->expires()?->dateTime()->format('Y-m-d H:i'))->toBe('2025-04-19 13:21')
+        ->and($allCookiesForDomain['foo']->name())->toBe('foo')
+        ->and($allCookiesForDomain['foo']->value())->toBe('one')
+        ->and($allCookiesForDomain['foo']->domain())->toBe('www.crwl.io')
+        ->and($allCookiesForDomain['foo']->maxAge())->toBe(86400)
+        ->and($allCookiesForDomain['foo']->path())->toBeNull()
+        ->and($allCookiesForDomain['foo']->secure())->toBeTrue()
+        ->and($allCookiesForDomain['foo']->httpOnly())->toBeTrue()
+        ->and($allCookiesForDomain['foo']->sameSite())->toBe('Strict')
         ->and($allCookiesForDomain['bar']->expires()?->dateTime()->format('Y-m-d H:i'))->toBe('2024-10-22 13:21')
+        ->and($allCookiesForDomain['bar']->name())->toBe('bar')
+        ->and($allCookiesForDomain['bar']->value())->toBe('two')
+        ->and($allCookiesForDomain['bar']->domain())->toBe('www.crwl.io')
+        ->and($allCookiesForDomain['bar']->maxAge())->toBeNull()
+        ->and($allCookiesForDomain['bar']->path())->toBe('/bar')
+        ->and($allCookiesForDomain['bar']->secure())->toBeFalse()
+        ->and($allCookiesForDomain['bar']->httpOnly())->toBeFalse()
+        ->and($allCookiesForDomain['bar']->sameSite())->toBe('Lax')
         ->and($allCookiesForDomain['baz']->expires()?->dateTime()->format('Y-m-d H:i'))->toBe('2025-11-25 13:21');
 });
 
