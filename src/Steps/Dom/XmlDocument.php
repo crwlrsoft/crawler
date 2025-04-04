eea09e62913763beff41b675d00bacd5f8991695
otsch <otsch@users.noreply.github.com>
2025-04-04 23:57:01 +0200

    Handle invalid characters in XML
    As sometimes, XML parsing errors occur because of characters that aren't
    valid within XML documents, the `XmlDocument` class now catches
    exceptions, tries to find and replace invalid characters (with
    transliterates or HTML entities) and retries parsing the document.
    If installed, it tries to find transliterates for characters, using the
    `voku/portable-ascii` composer package. Added the package to the
    `suggest` section in the composer.json file.

--- src/Steps/Dom/XmlDocument.php
+++ src/Steps/Dom/XmlDocument.php
@@ -5,6 +5,8 @@ namespace Crwlr\Crawler\Steps\Dom;
 use Crwlr\Utils\PhpVersion;
 use DOMNode;
 use Symfony\Component\DomCrawler\Crawler;
+use Throwable;
+use voku\helper\ASCII;
 
 /**
  * @method XmlElement|null querySelector(string $selector)
@@ -33,9 +35,49 @@ class XmlDocument extends DomDocument
     protected function makeDocumentInstance(string $source): object
     {
         if (PhpVersion::isAtLeast(8, 4)) {
-            return \Dom\XMLDocument::createFromString($source, LIBXML_NOERROR | LIBXML_NONET);
+            try {
+                return \Dom\XMLDocument::createFromString($source, LIBXML_NOERROR | LIBXML_NONET);
+            } catch (Throwable) {
+                $source = $this->replaceInvalidXmlCharacters($source);
+
+                try {
+                    return \Dom\XMLDocument::createFromString($source, LIBXML_NOERROR | LIBXML_NONET);
+                } catch (Throwable) {
+                } // If it fails again, try it with symfony DOM Crawler as fallback.
+            }
         }
 
-        return new Crawler($source);
+        $crawler = new Crawler($source);
+
+        if ($crawler->count() === 0) {
+            $source = $this->replaceInvalidXmlCharacters($source);
+
+            $crawler = new Crawler($source);
+        }
+
+        return $crawler;
+    }
+
+    /**
+     * Replace characters that aren't valid within XML documents
+     *
+     * Sometimes XML parsing errors occur because of characters that aren't valid within XML documents.
+     * Therefore, this method finds and replaces them with valid alternatives or HTML entities.
+     * For best results in those cases, please install the voku/portable-ascii composer package.
+     *
+     * @param string $value
+     * @return string
+     */
+    private function replaceInvalidXmlCharacters(string $value): string
+    {
+        return preg_replace_callback('/[^\x{9}\x{A}\x{D}\x{20}-\x{D7FF}\x{E000}-\x{FFFD}]/u', function ($match) {
+            $replacement = class_exists('voku\helper\ASCII') ? ASCII::to_transliterate($match[0]) : '?';
+
+            if ($replacement === '?') {
+                return '&#' . mb_ord($match[0]) . ';';
+            }
+
+            return $replacement;
+        }, $value) ?? $value;
     }
 }
